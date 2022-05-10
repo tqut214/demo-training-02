5bb3282a4d7d8d5827c63623278b0819ad87160c
tqut214 <78657896+tqut214@users.noreply.github.com>
2022-05-10 22:18:57 +0700

    VaribleStudy and StructStudy
    by QT

---
+++ StructStudy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class Program{
+    struct Character
+    {
+        public string Name;
+        public int HP;
+        public int MP;
+        public int level;
+        public int ATK;
+        public int DEF;
+    }
+    
+   
+    public static void Main(string[] args){
+        Character c = new Character();
+        c.Name = "QT";
+        c.HP = 100;
+        c.MP = 50;
+        c.level = 1;
+        c.ATK = 4;
+        c.DEF = 5;
+        Console.WriteLine("This is " + c.Name + " have " + c.HP + " HP");
+  
+    
+}
+}
---
+++ VaribleStudy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class Program{
+    public static void Main(string[] args){
+        int num = 10;
+        float mark = 9.6f;
+        string name = "Quan Tan";
+        Console.WriteLine(name);
+        Console.WriteLine(mark);
+        Console.WriteLine(num);
+}
+}
