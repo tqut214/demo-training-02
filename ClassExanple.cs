3f0e275c3aaa9dcaba42f520dd544384bd5d872e
tqut214 <78657896+tqut214@users.noreply.github.com>
2022-05-10 22:07:24 +0700

    Class Example
    by QT

---
+++ ClassExanple.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Hello, World!");
+Character c1 = new Character("Tan");
+c1.Introduction();
+public class Character
+{
+    public Character(string name)
+    {
+        this.Name = name;
+    }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    
+    public void Introduction()
+    {
+        Console.WriteLine("Hello my name is " + Name);
+
+    }
+}
