34a4fec4a99fd48387626ff53250f4b9ae3b1046
tqut214 <tqut214@gmail.com>
2022-05-11 15:20:08 +0700

    character move added

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-   
+    public Vector3 CharacterPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CharacterPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         Jump();
+        Move();
     }
     
     void Jump()
@@ -23,5 +24,12 @@ public class Character : MonoBehaviour
         {
             Debug.Log("Is Jump");
         }
+    }
+    void Move()
+    {
+        Vector3 moveXDir = new Vector3(Input.GetAxis("Vertical"),0,0);
+        CharacterPosition = moveXDir;
+        
+ 
     }
 }
